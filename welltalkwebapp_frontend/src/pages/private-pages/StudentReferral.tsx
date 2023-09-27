b10978a0ec1025102444b457d764c6b49f2b38f9
Benhur Guia <benhur.guia@cit.edu>
2023-09-27 20:16:22 +0800

    Student Referral
    Added Login Header

--- welltalkwebapp_frontend/src/pages/private-pages/StudentReferral.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/StudentReferral.tsx
@@ -1,7 +1,10 @@
+import LoggedinHeader from "@/components/LoggedinHeader";
 
 export const StudentReferral = () => {
+
   return (
     <>
+      <LoggedinHeader />
       <div className="ml-72">
         <h1 className="font-semibold">Student Referral</h1>
       </div>
