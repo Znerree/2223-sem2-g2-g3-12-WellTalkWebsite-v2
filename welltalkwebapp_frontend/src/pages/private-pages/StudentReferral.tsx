efe70f6b1f4aba4b7eb93cdd5e628e3e59b1165a
Benhur Guia <benhur.guia@cit.edu>
2023-09-27 19:52:39 +0800

    Student Referral Page Changes
    Side Bar and Login Header Removed in the Student Referral Component Under Pages < Private-pages < StudentReferral

--- welltalkwebapp_frontend/src/pages/private-pages/StudentReferral.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/StudentReferral.tsx
@@ -1,11 +1,7 @@
-import LoggedinHeader from "@/components/LoggedinHeader";
-import SidebarNav from "@/components/SidebarNav";
 
 export const StudentReferral = () => {
   return (
     <>
-      <SidebarNav />
-      <LoggedinHeader />
       <div className="ml-72">
         <h1 className="font-semibold">Student Referral</h1>
       </div>
