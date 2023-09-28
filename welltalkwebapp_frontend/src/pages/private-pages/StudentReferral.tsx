dcbd44e8082a6ce06f71527ba06f424b5209bd2e
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-09-29 00:23:58 +0800

    Gipatunga referral form ug naay pilianan others

--- welltalkwebapp_frontend/src/pages/private-pages/StudentReferral.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/StudentReferral.tsx
@@ -1,7 +1,8 @@
 import ReferralHeader from "@/components/ReferralHeader";
+import { useState } from "react";
 
 export const StudentReferral = () => {
-  //css style for input
+  // css style for input
   const inputStyle = {
     borderBottom: "2px solid #769EAB",
     background: "transparent",
@@ -11,39 +12,51 @@ export const StudentReferral = () => {
     marginBottom: "16px",
   };
 
+  const [referral, setReferral] = useState({});
+  const [showOtherInput, setShowOtherInput] = useState(false); // State to track if the "If other/s" input should be displayed
+
+  const handleReasonChange = (event: any) => {
+    const selectedReason = event.target.value;
+    if (selectedReason === "Other") {
+      setShowOtherInput(true);
+    } else {
+      setShowOtherInput(false);
+    }
+  };
+
   return (
     <>
       <ReferralHeader />
-      <div className=" ml-14 flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center w-full h-screen">
         <h1 className="font-semibold text-2xl">REFER SOMEONE NOW</h1>
-        <form className=" py-6 left-0 w-[300px] flex flex-col">
+        <form className="py-6 left-0 w-[300px] flex flex-col">
           <input
             type="text"
-            placeholder=" Input student name or ID number"
+            placeholder="Input student name or ID number"
             style={inputStyle}
             required
           />
           <select
             name="userType"
             required
-            className=" mb-3 text-gray-400 outline-none text-sm border-2 border-secondary rounded-md p-2"
+            className="mb-3 text-gray-400 outline-none text-sm border-2 border-secondary rounded-md p-2"
             defaultValue="label"
+            onChange={handleReasonChange}
           >
             <option disabled value="label" hidden>
               Reason/s for referrals
             </option>
-            <option value="Counselor">Sample 1</option>
-            <option value="Non-Counselor">Sample 2</option>
-            <option value="Non-Counselor">Sample 3</option>
+            <option value="Sample 1">Sample 1</option>
+            <option value="Sample 2">Sample 2</option>
+            <option value="Sample 3">Sample 3</option>
+            <option value="Other">Others, please specify</option>
           </select>
-          <input
-            type="text"
-            placeholder="If other/s"
-            style={inputStyle} 
-          />
+          {showOtherInput && (
+            <input type="text" placeholder="If other/s" style={inputStyle} />
+          )}
           <button
             type="submit"
-            className=" bg-primary rounded-full h-10 text-white hover:shadow-sm hover:shadow-primary mt-2"
+            className="bg-primary rounded-full h-10 text-white hover:shadow-sm hover:shadow-primary mt-2"
           >
             SUBMIT
           </button>
