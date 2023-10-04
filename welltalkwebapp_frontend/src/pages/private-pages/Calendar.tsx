c845143d2af0e542f921d58f082c8f7f91a9f2a1
jeiku14 <jake.prahinog@cit.edu>
2023-10-04 14:12:03 +0800

    added new form for schedule disseminating

--- welltalkwebapp_frontend/src/pages/private-pages/Calendar.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/Calendar.tsx
@@ -4,7 +4,7 @@ import ListOfAppointments from "@/components/Calendar/calendar-list-of-appointme
 import ReferredStudents from "@/components/Calendar/calendar-referred-students";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import axios from "@/api/axios";
-import { start } from "repl";
+import {HiSwitchHorizontal} from "react-icons/hi";
 
 type Student = {
   id: number;
@@ -39,6 +39,8 @@ const Calendar = () => {
   const [startDate, setStartDate] = useState("");
   const [startTime, setStartTime] = useState("");
 
+  const [showAnnounceSchedule, setShowAnnounceSchedule] = useState(false);
+
   useEffect(() => {
     const fetchStudents = async () => {
       const response = await axios.get("students");
@@ -58,9 +60,10 @@ const Calendar = () => {
     const inputValue = event.target.value;
     setValue(inputValue);
     setQuery(inputValue);
-    const filteredStudents = students.filter((student) =>
-      student.firstname.toLowerCase().includes(inputValue.toLowerCase()) ||
-      student.lastname.toLowerCase().startsWith(inputValue.toLowerCase())
+    const filteredStudents = students.filter(
+      (student) =>
+        student.firstname.toLowerCase().includes(inputValue.toLowerCase()) ||
+        student.lastname.toLowerCase().startsWith(inputValue.toLowerCase())
     );
     setResults(filteredStudents);
     console.log(value);
@@ -70,7 +73,7 @@ const Calendar = () => {
   };
 
   const handleStudentInput = (studentName: string) => {
-    console.log(value)
+    console.log(value);
     setValue(studentName);
     setResults([]);
     setQuery("");
@@ -79,10 +82,12 @@ const Calendar = () => {
       inputRef.current.value = studentName;
     }
 
-    const selectedStudent = students.find((student) => student.firstname + " " + student.lastname === studentName);
-    if(selectedStudent){
+    const selectedStudent = students.find(
+      (student) => student.firstname + " " + student.lastname === studentName
+    );
+    if (selectedStudent) {
       setStudentId(selectedStudent.id.toString());
-      console.log(selectedStudent.id)
+      console.log(selectedStudent.id);
     }
   };
   const inputRef = useRef<HTMLInputElement>(null);
@@ -90,7 +95,6 @@ const Calendar = () => {
   console.log(startTime);
   console.log(value);
   console.log(localStorage.getItem("counselorID"));
-
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -101,21 +105,25 @@ const Calendar = () => {
       start_date: `${startDate}T${startTime}`,
     };
     try {
-      const response = await axios.post('/appointments?student='+studentID, appointmentData, config);
+      const response = await axios.post(
+        "/appointments?student=" + studentID,
+        appointmentData,
+        config
+      );
       console.log(response.data);
-      alert('Appointment set successfully');
+      alert("Appointment set successfully");
       window.location.reload();
     } catch (error) {
-      console.error('Error creating appointment:', error);
+      console.error("Error creating appointment:", error);
     }
   };
 
   const handleSelectTime = (event: any) => {
     const time = event.target.value;
     console.log(time);
-    let [hours, minutes] = time.split(':');
-    let seconds = '00';
-    if (time.includes('pm')) {
+    let [hours, minutes] = time.split(":");
+    let seconds = "00";
+    if (time.includes("pm")) {
       hours = (parseInt(hours, 10) + 12).toString();
     }
     setStartTime(`${hours}:${minutes}:${seconds}`);
@@ -129,10 +137,49 @@ const Calendar = () => {
         <h1 className=" font-semibold">Calendar</h1>
         <div className=" w-80 rounded-lg shadow-2xl mt-10 px-2 flex flex-col border pb-3">
           {/* Set an appointment */}
-          <h1 className=" font-semibold text-md border-b sticky top-0 bg-white py-4 pl-2">
-            Set an Appointment
-          </h1>
-          <form className=" px-3 pt-3">
+          <div className="flex items-center justify-between border-b sticky">
+            <h1 className=" font-semibold text-md top-0 bg-white py-4 pl-2">
+              {showAnnounceSchedule
+                ? "Announce a Schedule"
+                : "Set an Appointment"}
+            </h1>
+
+            <HiSwitchHorizontal 
+              className="text-black-300 h-6 w-6 cursor-pointer"
+              onClick={() => setShowAnnounceSchedule(!showAnnounceSchedule)} />
+          </div>
+          {showAnnounceSchedule ?    
+          (<form className=" px-3 pt-3">
+          <label className=" text-sm text-gray-400">Date</label>
+          <input
+            name="date"
+            type="date"
+            style={inputStyle}
+            autoComplete="off"
+            required
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <label className=" text-sm text-gray-400">Time</label>
+          <input
+            name="time"
+            type="time"
+            style={inputStyle}
+            autoComplete="off"
+            required
+            value={startTime}
+            onChange={handleSelectTime}
+          />
+          <button
+            type="submit"
+            className=" text-sm bg-tertiary rounded-lg p-2 text-white hover:shadow-sm hover:shadow-secondary"
+            onClick={handleSubmit}
+          >
+            SET SCHEDULE
+          </button>
+        </form>)
+          :
+          (<form className=" px-3 pt-3">
           <div className="flex items-center">
             <input
               ref={inputRef}
@@ -156,9 +203,15 @@ const Calendar = () => {
                 <li
                   className=" w-full border p-2 cursor-pointer hover:bg-gray-100"
                   key={student.id}
-                  onClick={() => handleStudentInput(student.firstname + " " + student.lastname)}
+                  onClick={() =>
+                    handleStudentInput(
+                      student.firstname + " " + student.lastname
+                    )
+                  }
                 >
-                  <p className="text-sm bold">{student.firstname} {student.lastname}</p>
+                  <p className="text-sm bold">
+                    {student.firstname} {student.lastname}
+                  </p>
                   <p className="text-xs text-red-500">
                     Student id: {student.studentID}
                   </p>
@@ -166,34 +219,34 @@ const Calendar = () => {
               ))}
             </ul>
           )}
-            <label className=" text-sm text-gray-400">Date</label>
-            <input
-              name="date"
-              type="date"
-              style={inputStyle}
-              autoComplete="off"
-              required
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-            />
-            <label className=" text-sm text-gray-400">Time</label>
-            <input
-              name="time"
-              type="time"
-              style={inputStyle}
-              autoComplete="off"
-              required
-              value={startTime}
-              onChange={handleSelectTime}
-            />
-            <button
-              type="submit"
-              className=" text-sm bg-tertiary rounded-lg p-2 text-white hover:shadow-sm hover:shadow-secondary"
-              onClick={handleSubmit}
-            >
-              SET APPOINTMENT
-            </button>
-          </form>
+          <label className=" text-sm text-gray-400">Date</label>
+          <input
+            name="date"
+            type="date"
+            style={inputStyle}
+            autoComplete="off"
+            required
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <label className=" text-sm text-gray-400">Time</label>
+          <input
+            name="time"
+            type="time"
+            style={inputStyle}
+            autoComplete="off"
+            required
+            value={startTime}
+            onChange={handleSelectTime}
+          />
+          <button
+            type="submit"
+            className=" text-sm bg-tertiary rounded-lg p-2 text-white hover:shadow-sm hover:shadow-secondary"
+            onClick={handleSubmit}
+          >
+            SET APPOINTMENT
+          </button>
+        </form>)}
         </div>
 
         {/* List of appointments */}
