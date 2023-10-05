45edd6a4f8bbb07a9640eb4731100f245710940c
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-10-06 04:25:23 +0800

    added table to display all students

--- welltalkwebapp_frontend/src/pages/private-pages/Students.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/Students.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "@/api/axios";
+import { BsChatLeftDots } from "react-icons/bs";
 
 type StudentProps = {
   student: {
@@ -40,36 +41,27 @@ const Students = () => {
         <h1 className=" font-semibold">Students</h1>
         <div className=" w-ful">
           <table className=" mt-4">
-            <thead className="">
-              <tr className=" text-left">
-                <th className=" border border-secondary px-2">Student ID</th>
-                <th className=" border border-secondary px-2">Firstname</th>
-                <th className=" border border-secondary px-2">Lastname</th>
-                <th className=" border border-secondary px-2">Course</th>
-                <th className=" border border-secondary px-2">Year</th>
-                <th className=" border border-secondary px-2">Department</th>
+            <thead className=" text-center">
+              <tr>
+                <th className=" py-3 px-4">Student ID</th>
+                <th className=" py-3 px-4">Firstname</th>
+                <th className=" py-3 px-4">Lastname</th>
+                <th className=" py-3 px-4">Course</th>
+                <th className=" py-3 px-4">Year</th>
+                <th className=" py-3 px-4">Department</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className=" text-center">
               {students.map((student) => (
-                <tr key={student.studentID}>
-                  <td className=" border border-secondary px-2">
-                    {student.studentID}
-                  </td>
-                  <td className=" border border-secondary px-2">
-                    {student.firstname}
-                  </td>
-                  <td className=" border border-secondary px-2">
-                    {student.lastname}
-                  </td>
-                  <td className=" border border-secondary px-2">
-                    {student.course}
-                  </td>
-                  <td className=" border border-secondary px-2">
-                    {student.year}
-                  </td>
-                  <td className=" border border-secondary px-2">
-                    {student.department}
+                <tr key={student.studentID} className=" items-center">
+                  <td className=" py-2 px-4}">{student.studentID}</td>
+                  <td className=" py-2 px-4}">{student.firstname}</td>
+                  <td className=" py-2 px-4}">{student.lastname}</td>
+                  <td className=" py-2 px-4}">{student.course}</td>
+                  <td className=" py-2 px-4}">{student.year}</td>
+                  <td className=" py-2 px-4}">{student.department}</td>
+                  <td className=" py-2 px-4}">
+                    <BsChatLeftDots className=" text-blue-500 cursor-pointer" />
                   </td>
                 </tr>
               ))}
