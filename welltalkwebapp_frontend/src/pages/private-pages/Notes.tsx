ac77aa9e3d8ea8fbcd37fd8da421f8c1a8cc06bb
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-10-09 22:02:01 +0800

    added add note modal

--- welltalkwebapp_frontend/src/pages/private-pages/Notes.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/Notes.tsx
@@ -1,19 +1,46 @@
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 import { PiNotePencilBold } from "react-icons/pi";
 
 const Notes = () => {
+  const [showAddNote, setShowAddNote] = useState(false);
+
+  const openAddNoteModal = () => {
+    setShowAddNote(true);
+  };
+
+  const closeAddNoteModal = () => {
+    setShowAddNote(false);
+  };
+
   return (
     <>
       <div className=" ml-72 top-20 flex fixed">
-        <h1 className=" font-semibold sticky top-0 border-b">Notes</h1>
-        <div className=" mt-10 flex  overflow-y-auto h-screen">
-          <div className="grid grid-cols-4 gap-2 flex-grow">
-            <div className=" p-4 justify-center flex flex-col items-center w-60">
-              <button className=" flex items-center bg-primary shadow bg-opacity-90 p-3 rounded-full text-white gap-2 hover:bg-opacity-100">
-                <PiNotePencilBold size={15} className=" " />
-                <p>Add a note</p>
-              </button>
+        <div className=" flex justify-between w-full absolute">
+          <h1 className=" font-semibold">Notes</h1>
+          <button
+            onClick={openAddNoteModal}
+            className=" py-2 flex items-center px-3 bg-primary shadow bg-opacity-90 rounded-full text-white gap-2 hover:bg-opacity-100"
+          >
+            <PiNotePencilBold size={15} />
+            <p>Add a note</p>
+          </button>
+        </div>
+        {showAddNote && (
+          <div className=" absolute w-full  h-screen flex items-center justify-center">
+            <div className=" bg-white w-[500px] rounded-md h-[520px] shadow-xl border border-3">
+              <div className=" bg-primary rounded-tr-md rounded-tl-md py-6 px-3 items-center flex justify-between">
+                <h1 className=" text-white font-bold  text-lg">New note</h1>
+                <button onClick={closeAddNoteModal} className=" hover:text-tertiary">
+                  <IoMdClose />
+                </button>
+              </div>
             </div>
+          </div>
+        )}
 
+        <div className=" mt-12 border bg-gray-50 flex  overflow-y-auto h-screen">
+          <div className="grid grid-cols-4 gap-2 flex-grow">
             <div className="bg-white p-4 rounded-md shadow border w-72 h-72">
               <h2 className="text-lg font-semibold mb-2">Note 1</h2>
               <p>This is note 1 sddddddddddd.</p>
