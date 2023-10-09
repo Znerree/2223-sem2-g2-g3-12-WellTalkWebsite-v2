c06950b1662bcf2296577e04d6ea0e9d3b821d72
jeiku14 <jake.prahinog@cit.edu>
2023-10-09 19:09:59 +0800

    fixed minor bug in calendar-list-of-appointments

--- welltalkwebapp_frontend/src/components/Calendar/calendar-list-of-appointments.tsx
+++ welltalkwebapp_frontend/src/components/Calendar/calendar-list-of-appointments.tsx
@@ -31,8 +31,7 @@ interface Appointment {
 const ListOfAppointments = () => {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
   const [isTodayAppointmentsOpen, setTodayAppointmentsOpen] = useState(true);
-  const [isUpcomingAppointmentsOpen, setUpcomingAppointmentsOpen] =
-    useState(true);
+  const [isUpcomingAppointmentsOpen, setUpcomingAppointmentsOpen] = useState(true);
 
   const [isTodayDropdownOpen, setTodayDropdownOpen] = useState(false);
   const [isUpcomingDropdownOpen, setUpcomingDropdownOpen] = useState(false);
@@ -45,13 +44,7 @@ const ListOfAppointments = () => {
       .get<Appointment[]>("/myappointments", config)
       .then((response) => {
         setAppointments(
-          response.data
-            .sort(
-              (a, b) =>
-                new Date(a.start_date).getTime() -
-                new Date(b.start_date).getTime()
-            )
-            .filter((appointment) => !appointment.isDone)
+          response.data.sort((a, b) => new Date(a.start_date).getTime() - new Date(b.start_date).getTime()).filter((appointment) => !appointment.isDone)
         );
         console.log(response.data);
       })
@@ -62,14 +55,8 @@ const ListOfAppointments = () => {
 
   const today = new Date().toLocaleDateString();
 
-  const todayAppointments = appointments.filter(
-    (appointment) =>
-      new Date(appointment.start_date).toLocaleDateString() === today
-  );
-  const upcomingAppointments = appointments.filter(
-    (appointment) =>
-      new Date(appointment.start_date).toLocaleDateString() !== today
-  );
+  const todayAppointments = appointments.filter((appointment) => new Date(appointment.start_date).toLocaleDateString() === today);
+  const upcomingAppointments = appointments.filter((appointment) => new Date(appointment.start_date).toLocaleDateString() !== today);
 
   const expandTodayAppointments = () => {
     setTodayDropdownOpen(!isTodayDropdownOpen);
@@ -95,44 +82,33 @@ const ListOfAppointments = () => {
 
   return (
     <>
-      <h1 className=" font-semibold text-md border-b sticky top-0 bg-white py-4 pl-2">
-        My Appointments
-      </h1>
+      <h1 className=" font-semibold text-md border-b sticky top-0 bg-white py-4 pl-2">My Appointments</h1>
+      <div className="flex items-center justify-between pt-2">
+        <h2 className="font-sans text-md top-0 pl-2">Today's Appointments</h2>
 
-      {todayAppointments.length > 0 && (
+        <div className="flex items-center justify-between pr-2" onClick={expandTodayAppointments}>
+          {isTodayDropdownOpen ? (
+            <HiMiniPlusSmall
+              className="text-black-300 h-6 w-6 cursor-pointer"
+              onClick={() => {
+                setTodayAppointmentsOpen(!isTodayAppointmentsOpen);
+              }}
+            />
+          ) : (
+            <HiMiniMinusSmall
+              className="text-black-300 h-6 w-6 cursor-pointer"
+              onClick={() => {
+                setTodayAppointmentsOpen(!isTodayAppointmentsOpen);
+              }}
+            />
+          )}
+        </div>
+      </div>
+      {todayAppointments.length > 0 ? (
         <>
-          <div className="flex items-center justify-between pt-2">
-            <h2 className="font-sans text-md top-0 pl-2">
-              Today's Appointments
-            </h2>
-
-            <div
-              className="flex items-center justify-between pr-2"
-              onClick={expandTodayAppointments}
-            >
-              {isTodayDropdownOpen ? (
-                <HiMiniPlusSmall
-                  className="text-black-300 h-6 w-6 cursor-pointer"
-                  onClick={() => {
-                    setTodayAppointmentsOpen(!isTodayAppointmentsOpen);
-                  }}
-                />
-              ) : (
-                <HiMiniMinusSmall
-                  className="text-black-300 h-6 w-6 cursor-pointer"
-                  onClick={() => {
-                    setTodayAppointmentsOpen(!isTodayAppointmentsOpen);
-                  }}
-                />
-              )}
-            </div>
-          </div>
           <ul className={`p-2 ${isTodayAppointmentsOpen ? "" : "hidden"}`}>
             {todayAppointments.map((appointment, index) => (
-              <li
-                key={index}
-                className=" border-b px-2 rounded-md shadow-sm py-2 border mb-2 bg-tertiary"
-              >
+              <li key={index} className=" border-b px-2 rounded-md shadow-sm py-2 border mb-2 bg-tertiary">
                 <p className="text-white">
                   {appointment.student.firstname} {appointment.student.lastname}
                 </p>
@@ -141,84 +117,65 @@ const ListOfAppointments = () => {
                     <p className="text-gray-500 text-sm">
                       Course & Year:{" "}
                       <span className="text-primary">
-                        {appointment.student.course} -{" "}
-                        {appointment.student.year}
+                        {appointment.student.course} - {appointment.student.year}
                       </span>
                     </p>
                     <p className="text-gray-500 text-sm">
-                      Student ID:{" "}
-                      <span className="text-primary">
-                        {appointment.student.studentID}
-                      </span>
+                      Student ID: <span className="text-primary">{appointment.student.studentID}</span>
                     </p>
                   </div>
                   <div className="flex gap-4">
                     <p className=" text-gray-300 text-sm">
-                      Date:{" "}
-                      <span className="text-primary">
-                        {new Date(appointment.start_date).toLocaleDateString()}
-                      </span>
+                      Date: <span className="text-primary">{new Date(appointment.start_date).toLocaleDateString()}</span>
                     </p>
                     <p className=" text-gray-300 text-sm">
                       Start Time:{" "}
                       <span className="text-primary">
-                        {new Date(appointment.start_date).toLocaleTimeString(
-                          [],
-                          {
-                            hour: "numeric",
-                            minute: "numeric",
-                            hour12: true,
-                            second: undefined,
-                          }
-                        )}
+                        {new Date(appointment.start_date).toLocaleTimeString([], {
+                          hour: "numeric",
+                          minute: "numeric",
+                          hour12: true,
+                          second: undefined,
+                        })}
                       </span>
                     </p>
                   </div>
                 </div>
-                <button
-                  onClick={() => handleMarkAsDone(appointment.id)}
-                  className=" text-white text-xs bg-secondary p-1 rounded-md mt-2"
-                >
+                <button onClick={() => handleMarkAsDone(appointment.id)} className=" text-white text-xs bg-secondary p-1 rounded-md mt-2">
                   Mark as done
                 </button>
               </li>
             ))}
           </ul>
         </>
+      ) : (
+        <p className={`text-gray-500 text-sm p-2 ${isTodayAppointmentsOpen ? "" : "hidden"}`}>No appointments today.</p>       
       )}
-      {upcomingAppointments.length > 0 && (
+      <div className="flex items-center justify-between pt-2">
+        <h2 className="font-sans text-md top-0 pl-2">Upcoming Appointments</h2>
+        <div className="flex items-center justify-between pr-2" onClick={expandUpcomingAppointments}>
+          {isUpcomingDropdownOpen ? (
+            <HiMiniPlusSmall
+              className="text-black-300 h-6 w-6 cursor-pointer"
+              onClick={() => {
+                setUpcomingAppointmentsOpen(!isUpcomingAppointmentsOpen);
+              }}
+            />
+          ) : (
+            <HiMiniMinusSmall
+              className="text-black-300 h-6 w-6 cursor-pointer"
+              onClick={() => {
+                setUpcomingAppointmentsOpen(!isUpcomingAppointmentsOpen);
+              }}
+            />
+          )}
+        </div>
+      </div>
+      {upcomingAppointments.length > 0 ? (
         <>
-          <div className="flex items-center justify-between pt-2">
-            <h2 className="font-sans text-md top-0 pl-2">
-              Upcoming Appointments
-            </h2>
-            <div
-              className="flex items-center justify-between pr-2"
-              onClick={expandUpcomingAppointments}
-            >
-              {isUpcomingDropdownOpen ? (
-                <HiMiniPlusSmall
-                  className="text-black-300 h-6 w-6 cursor-pointer"
-                  onClick={() => {
-                    setUpcomingAppointmentsOpen(!isUpcomingAppointmentsOpen);
-                  }}
-                />
-              ) : (
-                <HiMiniMinusSmall
-                  className="text-black-300 h-6 w-6 cursor-pointer"
-                  onClick={() => {
-                    setUpcomingAppointmentsOpen(!isUpcomingAppointmentsOpen);
-                  }}
-                />
-              )}
-            </div>
-          </div>
           <ul className={`p-2 ${isUpcomingAppointmentsOpen ? "" : "hidden"}`}>
             {upcomingAppointments.map((appointment, index) => (
-              <li
-                key={index}
-                className=" border-b px-2 rounded-md shadow-sm py-2 border mb-2 bg-tertiary"
-              >
+              <li key={index} className=" border-b px-2 rounded-md shadow-sm py-2 border mb-2 bg-tertiary">
                 <p className="text-white">
                   {appointment.student.firstname} {appointment.student.lastname}
                 </p>
@@ -227,53 +184,39 @@ const ListOfAppointments = () => {
                     <p className="text-gray-500 text-sm">
                       Course & Year:{" "}
                       <span className="text-primary">
-                        {appointment.student.course} -{" "}
-                        {appointment.student.year}
+                        {appointment.student.course} - {appointment.student.year}
                       </span>
                     </p>
                     <p className="text-gray-500 text-sm">
-                      Student ID:{" "}
-                      <span className="text-primary">
-                        {appointment.student.studentID}
-                      </span>
+                      Student ID: <span className="text-primary">{appointment.student.studentID}</span>
                     </p>
                   </div>
                   <div className="flex gap-4">
                     <p className=" text-gray-300 text-sm">
-                      Date:{" "}
-                      <span className="text-primary">
-                        {new Date(appointment.start_date).toLocaleDateString()}
-                      </span>
+                      Date: <span className="text-primary">{new Date(appointment.start_date).toLocaleDateString()}</span>
                     </p>
                     <p className=" text-gray-300 text-sm">
                       Start Time:{" "}
                       <span className="text-primary">
-                        {new Date(appointment.start_date).toLocaleTimeString(
-                          [],
-                          {
-                            hour: "numeric",
-                            minute: "numeric",
-                            hour12: true,
-                            second: undefined,
-                          }
-                        )}
+                        {new Date(appointment.start_date).toLocaleTimeString([], {
+                          hour: "numeric",
+                          minute: "numeric",
+                          hour12: true,
+                          second: undefined,
+                        })}
                       </span>
                     </p>
                   </div>
                 </div>
-                <button
-                  onClick={() => handleMarkAsDone(appointment.id)}
-                  className=" text-white text-xs bg-secondary p-1 rounded-md mt-2"
-                >
+                <button onClick={() => handleMarkAsDone(appointment.id)} className=" text-white text-xs bg-secondary p-1 rounded-md mt-2">
                   Mark as done
                 </button>
               </li>
             ))}
           </ul>
         </>
-      )}
-      {todayAppointments.length === 0 && upcomingAppointments.length === 0 && (
-        <p className=" text-gray-500 text-sm p-2">No appointments yet.</p>
+      ) : (
+        <p className={`text-gray-500 text-sm p-2 ${isUpcomingAppointmentsOpen ? "" : "hidden"}`}>No upcoming appointments.</p>
       )}
     </>
   );
