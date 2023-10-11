88607ef53e01fedc5397d07d07a020edf7f5dfeb
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-10-12 03:17:13 +0800

    can post with photo or not, display own posts

--- welltalkwebapp_frontend/src/pages/private-pages/Home.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/Home.tsx
@@ -1,13 +1,17 @@
 import axios from "@/api/axios";
 import { useEffect, useState } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
 import { BsPersonCircle } from "react-icons/bs";
+import { FaRegComment } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 import { RiImageAddFill } from "react-icons/ri";
+import { MdPostAdd } from "react-icons/md";
 
 type PostsProps = {
   id: number;
   title: string;
   content: string;
+  photoContent: string;
   counselor: {
     id: number;
     firstName: string;
@@ -23,9 +27,10 @@ const Home = () => {
   const [imageFileName, setImageFileName] = useState<string>("");
   const [imageSrc, setImageSrc] = useState<string>("");
   const [counselor, setCounselor] = useState<any>({});
-  const [newPost, setNewPost] = useState<{ title: string; content: string }>({
+  const [newPost, setNewPost] = useState<{ title: string; content: string; photoData: File }>({
     title: "",
     content: "",
+    photoData: new File([], ""),
   });
 
   const getCounselor = async () => {
@@ -41,8 +46,9 @@ const Home = () => {
   const getAllPosts = async () => {
     try {
       const response = await axios.get("/posts");
+      const sortedPosts = response.data.sort((a: any, b: any) => b.id - a.id);
       console.log(response.data);
-      setAllPost(response.data);
+      setAllPost(sortedPosts);
     } catch (err) {
       console.error(err);
     }
@@ -54,8 +60,9 @@ const Home = () => {
     };
     try {
       const response = await axios.get("/myPosts", config);
+      const sortedUserPosts = response.data.sort((a: any, b: any) => b.id - a.id);
       console.log(response.data);
-      setMyPost(response.data);
+      setMyPost(sortedUserPosts);
     } catch (err) {
       console.error(err);
     }
@@ -84,7 +91,7 @@ const Home = () => {
     if (file) {
       setNewPost((prevPost) => ({
         ...prevPost,
-        image: file,
+        photoData: file,
       }));
 
       const reader = new FileReader();
@@ -105,15 +112,21 @@ const Home = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append("title", newPost.title);
+    formData.append("content", newPost.content);
+    formData.append("photoData", newPost.photoData);
+
     const config = {
       headers: { Authorization: `${localStorage.getItem("token")}` },
     };
     try {
-      const response = await axios.post("/posts", newPost, config);
+      const response = await axios.post("/posts/photo", formData, config);
       console.log(response);
       setNewPost({
         title: "",
         content: "",
+        photoData: new File([], ""),
       });
       alert("Post created!");
       window.location.reload();
@@ -132,21 +145,38 @@ const Home = () => {
 
   return (
     <>
-      <div className=" ml-72 top-20 absolute">
+      <div className=" ml-72 top-20 absolute flex flex-col">
         <h1 className=" font-semibold">Welcome to Welltalk!</h1>
-        <div className=" w-full">
-          <div className=" w-96 flex p-3 rounded-xl flex-col bg-secondary bg-opacity-50 shadow mt-2 sticky">
-            <input
-              type="text"
-              placeholder="Create a post"
-              className=" cursor-pointer w-full outline-none p-2 rounded-full bg-gray-200 hover:bg-gray-50 "
-              onClick={handleOpenPostForm}
-            />
-            <div onClick={handleOpenPostForm} className=" w-[70px] cursor-pointer mt-2 rounded-lg flex items-center gap-1 bg-secondary p-1 ">
-              <RiImageAddFill className=" text-white" />
-              <h1 className=" text-sm text-white">Photo</h1>
-            </div>
+        <div className=" w-[1240px]">
+          <div className=" px-5 flex justify-between">
+            <nav className=" flex gap-2">
+              <button
+                className={`rounded-full my-6 p-2 text-xs ${
+                  activeButton === "all" ? "bg-secondary text-white border-inherit" : "border-secondary border-2 bg-white text-secondary"
+                }`}
+                onClick={() => {
+                  setActiveButton("all");
+                }}
+              >
+                All Posts
+              </button>
+              <button
+                className={`rounded-full my-6 text-xs p-2 ${
+                  activeButton === "my" ? "bg-secondary text-white border-inherit" : "border-secondary border-2 bg-white text-secondary"
+                }`}
+                onClick={() => {
+                  setActiveButton("my");
+                }}
+              >
+                My Posts
+              </button>
+            </nav>
+            <button onClick={handleOpenPostForm} className=" border my-6 text-white px-2 bg-tertiary rounded-lg flex items-center gap-1 hover:bg-opacity-90">
+              <MdPostAdd />
+              Add new Post
+            </button>
           </div>
+
           {showPostForm && (
             <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-70 z-50">
               <div className="w-[550px] bg-white p-3 rounded-lg flex flex-col gap-3 relative">
@@ -181,11 +211,11 @@ const Home = () => {
                       <RiImageAddFill />
                       {imageSrc ? imageFileName : "Upload an Image"}
                     </label>
-                    <input id="imageUpload" type="file" name="image" className="hidden" onChange={handleImageUpload} />
+                    <input id="imageUpload" type="file" name="photoData" className="hidden" onChange={handleImageUpload} />
                   </div>
                   {imageSrc && (
                     <div className="mt-2">
-                      <img src={imageSrc} alt="Uploaded Image" className="max-w-full max-h-80 cursor-pointer" onClick={handleImageClick} />
+                      <img src={imageSrc} alt="Uploaded Image" className="max-w-auto max-h-auto cursor-pointer" onClick={handleImageClick} />
                     </div>
                   )}
 
@@ -196,32 +226,23 @@ const Home = () => {
               </div>
             </div>
           )}
-          <nav className=" mt-3 flex gap-2">
-            <button
-              className={`rounded-full p-2 ${
-                activeButton === "all" ? "bg-secondary text-white border-inherit" : "border-secondary border-2 bg-white text-secondary"
-              }`}
-              onClick={() => {
-                setActiveButton("all");
-              }}
-            >
-              All Posts
-            </button>
-            <button
-              className={`rounded-full p-2 ${
-                activeButton === "my" ? "bg-secondary text-white border-inherit" : "border-secondary border-2 bg-white text-secondary"
-              }`}
-              onClick={() => {
-                setActiveButton("my");
-              }}
-            >
-              My Posts
-            </button>
-          </nav>
+
           {activeButton === "all" && (
-            <div className=" w-[950px] flex flex-col items-center mt-2 gap-4 h-[400px] scroll-smooth overflow-auto">
+            <div className=" pr-3 w-full flex flex-col items-center gap-4 h-[550px] scroll-smooth overflow-auto border-t-2 py-2 bg-gray-50">
+              <div className=" bg-secondary bg-opacity-50 shadow p-3 rounded-lg w-[600px]">
+                <input
+                  type="text"
+                  placeholder="Create a post"
+                  className=" cursor-pointer w-full outline-none p-2 rounded-full bg-gray-200 hover:bg-gray-50 "
+                  onClick={handleOpenPostForm}
+                />
+                <div onClick={handleOpenPostForm} className=" w-[70px] cursor-pointer mt-2 rounded-lg flex items-center gap-1 bg-secondary p-1 ">
+                  <RiImageAddFill className=" text-white" />
+                  <h1 className=" text-sm text-white">Photo</h1>
+                </div>
+              </div>
               {allPost.map((post) => (
-                <div className=" w-[600px] rounded-lg bg-white border-2 shadow flex p-2 flex-col">
+                <div key={post.id} className=" w-[600px] rounded-lg bg-white border-2 shadow flex p-2 flex-col">
                   <div className=" flex items-center gap-2">
                     <BsPersonCircle size={20} className=" text-gray-500" />
                     <h1 className=" font-bold text-md">
@@ -230,6 +251,66 @@ const Home = () => {
                   </div>
                   <h2 className=" font-semibold mt-2">{post.title}</h2>
                   <p className=" mt-3">{post.content}</p>
+                  {post.photoContent ? (
+                    <img src={`data:image/jpeg;base64,${post.photoContent}`} alt="Posted Image" className=" shadow mt-3 max-w-auto max-h-auto cursor-pointer" />
+                  ) : null}
+                  <div className=" flex w-full justify-between items-center border-t border-b px-3 pb-2 border-gray-300 mt-3">
+                    <div className=" flex items-center gap-4 mt-2">
+                      <button className=" text-sm text-gray-500 hover:text-primary flex items-center gap-1">
+                        <AiOutlineHeart />
+                        <p>Like</p>
+                      </button>
+                      <button className=" text-sm text-gray-500 hover:text-primary flex items-center gap-1">
+                        <FaRegComment />
+                        <p>Comment</p>
+                      </button>
+                    </div>
+                    <p className=" text-xs text-gray-400">21 Likes</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+          {activeButton === "my" && (
+            <div className=" pr-3 w-full flex flex-col items-center gap-4 h-[500px] scroll-smooth overflow-auto border-t-2 py-2 bg-gray-50">
+              <div className=" bg-secondary bg-opacity-50 shadow p-3 rounded-lg w-[600px]">
+                <input
+                  type="text"
+                  placeholder="Create a post"
+                  className=" cursor-pointer w-full outline-none p-2 rounded-full bg-gray-200 hover:bg-gray-50 "
+                  onClick={handleOpenPostForm}
+                />
+                <div onClick={handleOpenPostForm} className=" w-[70px] cursor-pointer mt-2 rounded-lg flex items-center gap-1 bg-secondary p-1 ">
+                  <RiImageAddFill className=" text-white" />
+                  <h1 className=" text-sm text-white">Photo</h1>
+                </div>
+              </div>
+              {myPost.map((post) => (
+                <div key={post.id} className=" w-[600px] rounded-lg bg-white border-2 shadow flex p-2 flex-col">
+                  <div className=" flex items-center gap-2">
+                    <BsPersonCircle size={20} className=" text-gray-500" />
+                    <h1 className=" font-bold text-md">
+                      {post.counselor.firstName} {post.counselor.lastName}
+                    </h1>
+                  </div>
+                  <h2 className=" font-semibold mt-2">{post.title}</h2>
+                  <p className=" mt-3">{post.content}</p>
+                  {post.photoContent ? (
+                    <img src={`data:image/jpeg;base64,${post.photoContent}`} alt="Posted Image" className=" shadow mt-3 max-w-auto max-h-auto cursor-pointer" />
+                  ) : null}
+                  <div className=" flex w-full justify-between items-center">
+                    <div className=" flex items-center gap-4 mt-2">
+                      <button className=" text-sm text-gray-500 hover:text-primary flex items-center gap-1">
+                        <AiOutlineHeart />
+                        <p>Like</p>
+                      </button>
+                      <button className=" text-sm text-gray-500 hover:text-primary flex items-center gap-1">
+                        <FaRegComment />
+                        <p>Comment</p>
+                      </button>
+                    </div>
+                    <p className=" text-xs text-gray-400">21 Likes</p>
+                  </div>
                 </div>
               ))}
             </div>
