66cfc2b6783ce0b6f2c9c2ad6b6de0e12ec24076
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-10-13 17:02:11 +0800

    delete is ok

--- welltalkwebapp_frontend/src/pages/private-pages/Home.tsx
+++ welltalkwebapp_frontend/src/pages/private-pages/Home.tsx
@@ -1,11 +1,11 @@
 import axios from "@/api/axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BsPersonCircle } from "react-icons/bs";
 import { FaRegComment } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
-import { RiImageAddFill } from "react-icons/ri";
-import { MdPostAdd } from "react-icons/md";
+import { RiDeleteBin5Line, RiImageAddFill } from "react-icons/ri";
+import { MdOutlineModeEdit, MdPostAdd } from "react-icons/md";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 type PostsProps = {
@@ -28,11 +28,15 @@ const Home = () => {
   const [imageFileName, setImageFileName] = useState<string>("");
   const [imageSrc, setImageSrc] = useState<string>("");
   const [counselor, setCounselor] = useState<any>({});
+  const [showPostOptions, setShowPostOptions] = useState<number | null>(null);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [newPost, setNewPost] = useState<{ title: string; content: string; photoData: File }>({
     title: "",
     content: "",
     photoData: new File([], ""),
   });
+
+  const postOptionsRef = useRef<HTMLDivElement>(null);
 
   const getCounselor = async () => {
     try {
@@ -151,12 +155,57 @@ const Home = () => {
     }
   };
 
+  const handleOpenOptions = (postId: number) => {
+    setShowPostOptions(postId);
+    console.log(postId);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (postOptionsRef.current && !postOptionsRef.current.contains(event.target as Node)) {
+        setShowPostOptions(null);
+      }
+    };
+
+    if (showPostOptions) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showPostOptions]);
+
+  const handleOpenConfirmDelete = (postId: number) => {
+    myPost.find((post) => post.id === postId);
+    setShowDeleteModal(true);
+  };
+
+  const handleCancelDelete = () => {
+    setShowDeleteModal(false);
+  };
+
   const handleOpenPostForm = () => {
     setShowPostForm(true);
   };
 
   const handleClosePostForm = () => {
     setShowPostForm(false);
+  };
+
+  const handleDeletePost = async (id: number) => {
+    const config = {
+      headers: { Authorization: `${localStorage.getItem("token")}` },
+    };
+    try {
+      const response = await axios.delete(`/posts/${id}`, config);
+      console.log(response);
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
@@ -192,9 +241,9 @@ const Home = () => {
               Add new Post
             </button>
           </div>
-
+          {/* <div className="fixed inset-0 bg-gray-900 bg-opacity-70 flex items-center justify-center"> */}
           {showPostForm && (
-            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-70 z-50">
+            <div className="fixed inset-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-70">
               <div className="w-[550px] max-h-[500px] overflow-auto bg-white p-3 rounded-lg flex flex-col gap-3 relative">
                 <button onClick={handleClosePostForm} className="text-tertiary hover:text-primary text-xl px-4 py-2 absolute top-0 right-0">
                   <IoMdClose />
@@ -292,7 +341,7 @@ const Home = () => {
             </div>
           )}
           {activeButton === "my" && (
-            <div className=" pr-3 w-full flex flex-col items-center gap-4 h-[500px] scroll-smooth overflow-auto border-t-2 py-2 bg-gray-50">
+            <div className=" pr-3 w-full flex flex-col items-center gap-4 h-[550px] scroll-smooth overflow-auto border-t-2 py-2 bg-gray-50">
               <div className=" bg-secondary bg-opacity-50 shadow p-3 rounded-lg w-[600px]">
                 <input
                   type="text"
@@ -307,12 +356,51 @@ const Home = () => {
               </div>
               {myPost.map((post) => (
                 <div key={post.id} className=" w-[600px] rounded-lg bg-white border-2 shadow flex p-2 flex-col">
-                  <div className=" flex items-center gap-2">
-                    <BsPersonCircle size={20} className=" text-gray-500" />
-                    <h1 className=" font-bold text-md">
-                      {post.counselor.firstName} {post.counselor.lastName}
-                    </h1>
-                    <HiOutlineDotsHorizontal className=" text-gray-500 ml-auto cursor-pointer" />
+                  <div className=" flex justify-between items-center">
+                    <div className=" flex items-center gap-2">
+                      <BsPersonCircle size={20} className=" text-gray-500" />
+                      <h1 className=" font-bold text-md">
+                        {post.counselor.firstName} {post.counselor.lastName}
+                      </h1>
+                    </div>
+                    <div className="relative flex items-center">
+                      <HiOutlineDotsHorizontal onClick={() => handleOpenOptions(post.id)} className="text-gray-500 cursor-pointer" />
+                      {showPostOptions === post.id && (
+                        <div ref={postOptionsRef} key={post.id} className="absolute right-0 top-5 bg-white shadow border rounded-md px-2 py-2">
+                          <div className="text-sm text-tertiary cursor-pointer flex items-center gap-1 p-1 mb-2 rounded-lg hover:bg-blue-500 hover:text-white">
+                            <MdOutlineModeEdit size={15} />
+                            <span className=" flex gap-1">
+                              <span>Edit</span>
+                              <span>post</span>
+                            </span>
+                          </div>
+                          <p className="w-full border"></p>
+                          <div className="text-sm text-tertiary cursor-pointer flex items-center gap-1 p-1 mt-2 rounded-lg hover:bg-red-500 hover:text-white">
+                            <RiDeleteBin5Line size={15} />
+                            <p onClick={() => handleOpenConfirmDelete(post.id)} className=" flex gap-1">
+                              <span>Delete</span>
+                              <span>post</span>
+                            </p>
+                            {showDeleteModal && (
+                              <div className="fixed inset-0 bg-gray-900 bg-opacity-70 flex items-center justify-center">
+                                <div className="bg-white rounded-lg p-4 flex flex-col">
+                                  <h1 className=" text-black font-semibold text-lg">Delete</h1>
+                                  <p className=" text-gray-500  border-t border-b py-4 px-1 my-3">Are you sure you want to delete this post?</p>
+                                  <div className="flex justify-end mt-4">
+                                    <button onClick={handleCancelDelete} className="text-sm text-gray-500 hover:text-primary mr-2">
+                                      Cancel
+                                    </button>
+                                    <button onClick={() => handleDeletePost(post.id)} className="text-sm text-red-500 hover:text-red-700">
+                                      Delete
+                                    </button>
+                                  </div>
+                                </div>
+                              </div>
+                            )}
+                          </div>
+                        </div>
+                      )}
+                    </div>
                   </div>
                   <h2 className=" font-semibold mt-2">{post.title}</h2>
                   <p className=" mt-3 text-justify">{post.content}</p>
@@ -323,7 +411,7 @@ const Home = () => {
                       className=" shadow mt-3 max-w-auto max-h-auto cursor-pointer rounded-md"
                     />
                   ) : null}
-                  <div className=" flex w-full justify-between items-center">
+                  <div className=" flex w-full justify-between items-center border-t border-b px-3 pb-2 border-gray-300 mt-3">
                     <div className=" flex items-center gap-4 mt-2">
                       <button className=" text-sm text-gray-500 hover:text-primary flex items-center gap-1">
                         <AiOutlineHeart />
