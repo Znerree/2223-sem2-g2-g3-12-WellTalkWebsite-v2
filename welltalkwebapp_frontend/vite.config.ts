a42d8cea63c94ea5d584221dfb7d4beccf0517d7
Jake Errenz Batulan <114793116+Znerree@users.noreply.github.com>
2023-10-23 18:13:28 +0800

    qr code? to test in phone?

--- welltalkwebapp_frontend/vite.config.ts
+++ welltalkwebapp_frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { qrcode } from "vite-plugin-qrcode";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), qrcode()],
+  server: {
+    host: "true",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
